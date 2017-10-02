f2c6c358e8846b0d71054c085fe8be75bd61ec3b
SpecialBomb <hawkingfan2000@gmail.com>
2017-10-02 15:19:15 -0400

    simple formatting added to compile script

--- slingbase/mapsource/compile.sh
+++ slingbase/mapsource/compile.sh
@@ -4,8 +4,11 @@ echo "Type the name of the map you would like to compile without extensions:"
 
 read map
 
+echo "-----------QBSP-----------"
 qbsp $map.map $map.bsp
+echo "-----------LIGHT-----------"
 light -extra4 -dirt -dirtdebug -dirtdepth 128 $map.bsp
+echo "-----------VIS-----------"
 vis $map.bsp
 
 echo "Done!"
